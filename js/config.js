ba92817aa061d88f36c78fb9bb0f7c3ccc8fe7cb
genius-0963 <geniusthegreat0963@gmail.com>
2026-05-06 15:42:49 +0530

    feat: extract configuration to separate module

---
+++ js/config.js
@@ -0,0 +1,41 @@
+/**
+ * DMRC Visualizer Configuration
+ * Contains line colors and mapping constants
+ */
+
+export const LINE_COLORS = {
+    'Yellow': 0xFFD700,
+    'Blue': 0x0066FF,
+    'Magenta': 0xFF00FF,
+    'Green': 0x00FF00,
+    'Red': 0xFF0000,
+    'Violet': 0x8B00FF,
+    'Rapid': 0xFF8C00,
+    'Pink': 0xFF69B4,
+    'Orange': 0xFFA500,
+    'Aqua': 0x00FFFF,
+    'Gray': 0x808080
+};
+
+export const LINE_COLOR_HEX = {
+    'Yellow': '#FFD700',
+    'Blue': '#0066FF',
+    'Magenta': '#FF00FF',
+    'Green': '#00FF00',
+    'Red': '#FF0000',
+    'Violet': '#8B00FF',
+    'Rapid': '#FF8C00',
+    'Pink': '#FF69B4',
+    'Orange': '#FFA500',
+    'Aqua': '#00FFFF',
+    'Gray': '#808080'
+};
+
+export const SCENE_CONFIG = {
+    scale: 400,
+    offsetX: 77.15,
+    offsetZ: 28.6,
+    cameraStart: { x: 30, y: 25, z: 30 },
+    fogColor: 0x0a0a0f,
+    backgroundColor: 0x0a0a0f
+};
